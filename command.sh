c946f03ca87dbb97f7ff4e581f0e79bd114cd3df
Raeshelle Rose <raeshellerose@outlook.com>
2022-05-02 12:29:19 -0500

    list of commands

---
+++ command.sh
@@ -0,0 +1,12 @@
+# npx create-react-app propeller-app
+# cd propeller-app
+# git add .
+# git branch update_logo
+# git checkout main
+# git add .
+# gH repo create [blackbird-propeller-app] –public
+# code .
+# git add .
+# git commit -m “I updated the logo and link in app.js”
+# gh pr create –base main
+# gh pr merge
